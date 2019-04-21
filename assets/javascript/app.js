ee32018b6aa7463be169be4c1bde3674debb10b0
Rubentba <rubenatorresbeltran@gmail.com>
2019-04-20 23:25:25 -0400

    Added reset.css and initial style.css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,43 @@
-var games = ["Dark Souls", "Hollow Knight", "Darkest Dungeon", "The Legend of Zelda: Ocarina of Time"]
+var games = ['Dark Souls', 'Hollow Knight', 'Darkest Dungeon', 'The Legend of Zelda: Ocarina of Time']
 
 function displayGameGifs() {
 
-    var gif = $(this).attr("data-name"),
-        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
-        gif + "&api_key=jHW5YXf5zGdohyBV8cadbN1RZIhXfQ3i&limit=10"
+    var gif = $(this).attr('data-name'),
+        queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + 
+        gif + '&api_key=jHW5YXf5zGdohyBV8cadbN1RZIhXfQ3i&limit=10'
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: 'GET'
     }).then(function(response) {
-        $("button").on("click", function() {
+        $('button').on('click', function() {
             var results = response.data
 
             for (var i = 0; i < results.length; i++) {
 
-            var gameDiv = $("<div>")
+            var gameDiv = $('<div>')
                           .append(p)
                           .append(gameImage),
-                p = $("<p>").text("Rating: " + results[i].rating),
-                gameImage = $("<img>")
-                            .attr("data-state", "still")
-                            .attr("src", results[i].images.fixed_height_still.url)
-                            .attr("data-animate", results[i].images.fixed_height.url)
-                            .attr("data-still", results[i].images.fixed_height_still.url)
-                            .addClass("gifImage")
+                p = $('<p>').text('Rating: ' + results[i].rating),
+                gameImage = $('<img>')
+                            .attr('data-state', 'still')
+                            .attr('src', results[i].images.fixed_height_still.url)
+                            .attr('data-animate', results[i].images.fixed_height.url)
+                            .attr('data-still', results[i].images.fixed_height_still.url)
+                            .addClass('gifImage')
 
-            $("#gifContainer").prepend(gameDiv)
+            $('#gifContainer').prepend(gameDiv)
             }
 
-            $('.gifImage').on("click", function() {
+            $('.gifImage').on('click', function() {
 
-                var state = $(".gifImage").attr("data-state");
+                var state = $('.gifImage').attr('data-state');
 
-                if (state === "still") {
-                  $(this).attr("src", $(this).attr("data-animate"));
-                  $(this).attr("data-state", "animate");
+                if (state === 'still') {
+                  $(this).attr('src', $(this).attr('data-animate'));
+                  $(this).attr('data-state', 'animate');
                 } else {
-                  $(this).attr("src", $(this).attr("data-still"));
-                  $(this).attr("data-state", "still");
+                  $(this).attr('src', $(this).attr('data-still'));
+                  $(this).attr('data-state', 'still');
                 }
                 console.log(this)
             });
@@ -47,22 +47,22 @@ function displayGameGifs() {
 
 function generateButtons() {
 
-    $("#btns").empty()
+    $('#btns').empty()
 
     for (var i = 0; i < games.length; i++) {
 
-        var btn = $("<button>").addClass("game")
-                  .attr("data-name", games[i])
+        var btn = $('<button>').addClass('game')
+                  .attr('data-name', games[i])
                   .text(games[i]);
 
-        $("#btns").append(btn);
+        $('#btns').append(btn);
     }
 }
 
-$("#submitBtn").on("click", function(event) {
+$('#submitBtn').on('click', function(event) {
     event.preventDefault()
 
-    gif = $("#gif-input").val().trim()
+    gif = $('#gif-input').val().trim()
 
     games.push(gif)
     console.log(games)
@@ -70,6 +70,6 @@ $("#submitBtn").on("click", function(event) {
     generateButtons()
 });
 
-$(document).on("click", ".game", displayGameGifs)
+$(document).on('click', '.game', displayGameGifs)
 
 generateButtons()
